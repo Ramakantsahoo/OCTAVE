4f7d5cd97568b2cfa5e4c94a9ee67e4af16c289f
Ramakant <98227695+Ramakantsahoo@users.noreply.github.com>
2022-11-20 00:21:34 +0530

    Add files via upload

---
+++ sorting.m
@@ -0,0 +1,27 @@
+# sort elements of an array.
+arr=input("Enter the elements: ");
+arr
+len=length(arr);
+for i=1:len
+  for j=i+1:len
+    if(arr(i)>arr(j))
+      temp=arr(i);
+      arr(i)=arr(j);
+      arr(j)=temp;
+    endif
+  endfor
+endfor
+printf("After sorting: \n");
+for i=1:len
+    printf("%d ",arr(i));
+endfor
+printf("\n");
+
+# Command window
+# Enter the elements: [4,6,2,7,8]
+# arr =
+
+#   4   6   2   7   8
+
+# After sorting:
+# 2 4 6 7 8
