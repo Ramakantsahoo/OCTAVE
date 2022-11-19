624a1f8c195993f08976c7ca20ab430fec32695c
Ramakant <98227695+Ramakantsahoo@users.noreply.github.com>
2022-11-20 00:20:51 +0530

    Add files via upload

---
+++ matrix_multiplication.m
@@ -0,0 +1,26 @@
+# Create two matrix and multiply them.
+mat1=[1,2,3,4;3,4,5,6;,5,6,7,8];
+mat2=[2,4,5;45,6,7;45,6,7;4,5,6];
+mat1
+mat2
+mult=mat1*mat2;
+mult
+# Command window
+# mat1 =
+
+#   1   2   3   4
+#   3   4   5   6
+#   5   6   7   8
+
+# mat2 =
+
+#    2    4    5
+#   45    6    7
+#   45    6    7
+#    4    5    6
+
+# mult =
+
+#   243    54    64
+#   435    96   114
+#   627   138   164
