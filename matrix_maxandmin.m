2c475c296477f9a61cf2f35f0891d7f6f46e669c
Ramakant <98227695+Ramakantsahoo@users.noreply.github.com>
2022-11-20 00:20:35 +0530

    Add files via upload

---
+++ matrix_maxandmin.m
@@ -0,0 +1,33 @@
+# Minimum and maximum element of rows and columns of the matrix.
+mat=[1 2 25 5;4 6 8 9;67 77 8 6];
+mat
+max(mat)
+min(mat)
+max(mat,[],2)
+max(mat,[],2)
+# Command window
+# mat =
+
+#    1    2   25    5
+#    4    6    8    9
+#    67   77    8    6
+
+# ans =
+
+#   67   77   25    9
+
+# ans =
+
+#   1   2   8   5
+
+# ans =
+
+#   25
+#    9
+#   77
+
+# ans =
+
+#   25
+#    9
+#   77
