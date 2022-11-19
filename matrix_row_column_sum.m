ed68cab8059dc38b752b68aed5e2cb006962a00b
Ramakant <98227695+Ramakantsahoo@users.noreply.github.com>
2022-11-20 00:21:10 +0530

    Add files via upload

---
+++ matrix_row_column_sum.m
@@ -0,0 +1,23 @@
+# Initialize a 4*4 matrix and calculate sum of element of row and column.
+a=[1,2,3,54;4,5,6,7;5,4,3,2;5,6,7,8];
+a
+sum(a,1)
+sum(a,2)
+
+# Command window
+# a =
+
+#    1    2    3   54
+#    4    5    6    7
+#    5    4    3    2
+#    5    6    7    8
+
+# ans =
+
+#   15   17   19   71
+
+# ans =
+
+#   22
+#   14
+#   26
